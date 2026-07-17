05205b2a6415ffc7119a17399e30bc88a2f4db78
VtoosTechnologies <vtoos.software@gmail.com>
2026-07-17 19:16:15 +0530

    Implement login function using Firebase authentication

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,15 @@
+import { auth } from "./firebase.js";
 
+import {
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+export async function login(email, password){
+
+    return await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+    );
+
+}
