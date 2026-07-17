b872869d112a62352f7e13d4450c9681e7b0d68e
VtoosTechnologies <vtoos.software@gmail.com>
2026-07-17 18:24:04 +0530

    Implement login validation on button click
    Add login button click event listener for validation.

--- js/login.js
+++ js/login.js
@@ -1,1 +1,21 @@
+const loginBtn = document.getElementById("loginBtn");
 
+loginBtn.addEventListener("click", () => {
+
+    const email =
+    document.getElementById("email").value.trim();
+
+    const password =
+    document.getElementById("password").value.trim();
+
+    if(email === "" || password === ""){
+
+        alert("Please enter Email and Password");
+
+        return;
+
+    }
+
+    alert("Validation Success");
+
+});
