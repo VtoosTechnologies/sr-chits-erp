1f6bec12803629ce558ce732d71e5b1bedf0c8dd
VtoosTechnologies <vtoos.software@gmail.com>
2026-07-20 12:33:29 +0530

    Format date for prize payment display

--- js/prize-payment.js
+++ js/prize-payment.js
@@ -141,9 +141,19 @@ document.createElement("option");
 
 option.value = doc.id;
 
-// Temporary Display
+const date = new Date(data.startDate);
+
+const day = String(date.getDate()).padStart(2,"0");
+
+const month = date.toLocaleString("en-US",{
+month:"short"
+});
+
+const year =
+String(date.getFullYear()).slice(-2);
+
 option.textContent =
-data.groupName;
+`${data.groupCode} | ${day}-${month}'${year}`;
 
 group.appendChild(option);
 
