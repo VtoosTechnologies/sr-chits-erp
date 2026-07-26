44657edb5fbb44ec74202182b4b964be52a22050
VtoosTechnologies <vtoos.software@gmail.com>
2026-07-26 07:02:58 +0530

    Fix import path for firebase module

--- js/collection-report.js
+++ js/collection-report.js
@@ -4,7 +4,7 @@ Collection Report
 Part - 1
 ==================================================*/
 
-import { db } from "../firebase.js";
+import { db } from "./firebase.js";
 
 import {
 collection,
